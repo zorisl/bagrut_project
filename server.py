fc17b3fbea0bc3e17124cf0b840f219707d4a670
zorisl <zorislavuter@gmail.com>
2023-06-08 08:28:33 +0300

    3.0 update

--- server.py
+++ server.py
@@ -1,34 +1,82 @@
+import multiprocessing
 import socket
+import threading
 
-HOST = 'localhost'
-PORT = 5000
+from users import *
 
-users = {'admin': 'password'}
+cores = multiprocessing.cpu_count()
+print(cores)
 
-def authenticate_user(username, password):
-    if username in users and users[username] == password:
-        return True
-    else:
-        return False
+class Server(object):
+   def __init__(self, ip, port):
+       self.ip = ip
+       self.port = port
+       self.count = 0
+       self.running=True
+       self.userDb = User()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print(f'Server listening on {HOST}:{PORT}...')
+   def start(self):
+       try:
+           print('server starting up on ip %s port %s' % (self.ip, self.port))
+           # Create a TCP/IP socket
+           self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+           self.sock.bind((self.ip, self.port))
+           self.sock.listen(3)
 
-    while True:
-        conn, addr = s.accept()
-        print(f'Client connected from {addr}')
+           while True:
+               print('waiting for a new client')
+               clientSocket, client_addresses = self.sock.accept()
+               print('new client entered')
+               clientSocket.send('Hello this is server'.encode())
+               self.count += 1
+               print(self.count)
+               # implement here your main logic
+               self.handleClient(clientSocket, self.count)
 
-        with conn:
-            while True:
-                data = conn.recv(1024).decode()
-                if not data:
-                    break
-                username, password = data.split(',')
-                if authenticate_user(username, password):
-                    conn.sendall(b'Welcome,' + username.encode())
-                else:
-                    conn.sendall(b'Invalid username or password')
+       except socket.error as e:
+           print(e)
+
+   def handleClient(self, clientSock, current):
+       client_handler = threading.Thread(target=self.handle_client_connection, args=(clientSock, current,))
+       client_handler.start()
+
+   def handle_client_connection(self, client_socket, current):
+       not_crash = True
+       print(not_crash)
+       while self.running:
+
+           while not_crash:
+               try:
+                   server_data = client_socket.recv(1024).decode('utf-8')
+                   #insert,username,password
+                   arr = server_data.split(",")
+                   print(server_data)
+                   if arr!=None and arr[0]=="register" and len(arr)==3:
+                       print("register user")
+                       print(arr)
+                       server_data=self.userDb.insert_user(arr[1], arr[2])
+                       print("sertver data:",server_data)
+                       if server_data:
+                           client_socket.send("success register".encode())
+                       elif server_data:
+                           client_socket.send("failed register".encode())
+                   if arr!=None and arr[0]=="login" and len(arr)==3:
+                        server_data = self.userDb.return_user_by_username(arr[1])
+                   elif arr!=None and arr[0] == "get_all_users" and len(arr)==1:
+                       print("get_all_users")
+                       server_data=self.userDb.select_all_users()
+                       server_data = ",".join(server_data) # convert data to string
+                   else:
+                       server_data="False"
+               except:
+                   print("error")
+                   not_crash = False
+                   break
+
+if __name__ == '__main__':
+   ip = '127.0.0.1'
+   port = 1802
+   s = Server(ip, port)
+   s.start()
 
 
